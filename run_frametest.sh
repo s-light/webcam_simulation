0482875b8d45ab34cde451a27491b0be9af09c2f
s-light <github@s-light.eu>
2017-05-15 09:51:58 +0200

    update run_frametest to output to 40..42

--- run_frametest.sh
+++ run_frametest.sh
@@ -1,1 +1,4 @@
-ffmpeg -loop 1 -re -i frame_test.png -f v4l2 -vcodec rawvideo -pix_fmt rgb24 -framerate 30 /dev/video40
+ffmpeg -loop 1 -re -i frame_test.png\
+    -f v4l2 -vcodec rawvideo -pix_fmt rgb24 -framerate 30 /dev/video40\
+    -f v4l2 -vcodec rawvideo -pix_fmt rgb24 -framerate 30 /dev/video41\
+    -f v4l2 -vcodec rawvideo -pix_fmt rgb24 -framerate 30 /dev/video42\
