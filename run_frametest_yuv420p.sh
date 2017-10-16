50b7a49a2723c1a9ad9f81126e317e12abcd96bb
s-light <github@s-light.eu>
2017-10-17 01:13:44 +0200

    clean up

--- run_frametest_yuv420p.sh
+++ run_frametest_yuv420p.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
+
 ffmpeg -loop 1 -re -i frame_test.png\
-    -f v4l2 -vcodec rawvideo -pix_fmt yuv420p -framerate 30 /dev/video50\
-    -f v4l2 -vcodec rawvideo -pix_fmt yuv420p -framerate 30 /dev/video51\
-    -f v4l2 -vcodec rawvideo -pix_fmt yuv420p -framerate 30 /dev/video52\
-    -f v4l2 -vcodec rawvideo -pix_fmt yuv420p -framerate 30 /dev/video53\
-    -f v4l2 -vcodec rawvideo -pix_fmt yuv420p -framerate 30 /dev/video54\
-    -f v4l2 -vcodec rawvideo -pix_fmt yuv420p -framerate 30 /dev/video55\
+    -f v4l2 -vcodec rawvideo -pix_fmt yuv420p -framerate 30 /dev/video40\
+    -f v4l2 -vcodec rawvideo -pix_fmt yuv420p -framerate 30 /dev/video41\
+    -f v4l2 -vcodec rawvideo -pix_fmt yuv422p -framerate 30 /dev/video42\
+    -f v4l2 -vcodec rawvideo -pix_fmt yuv420p -framerate 30 /dev/video43\
--- setup_v4l2loopback.sh
+++ setup_v4l2loopback.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
+# sudo modprobe v4l2loopback \
+#     video_nr=40,41,42,43,44,45,50 \
+#     card_label="IntLB0 rgb8", "IntLB1 rgb8", "IntLB2 rgb8", "IntLB3 rgb8", "IntLB4 rgb8", "IntLB5 rgb8", "IntLB6 yuv420p"
+
 sudo modprobe v4l2loopback \
-    video_nr=40,41,42,43,44,45,50,51,52,53,54,55,60,61 \
-    card_label="IntLB0 rgb8","IntLB1 rgb8","IntLB2 rgb8","IntLB3 rgb8","IntLB4 rgb8","IntLB5 rgb8","IntLB6 yuv420p","IntLB7 yuv420p","IntLB8 yuv420p","IntLB9 yuv420p","IntLB10 yuv420p","IntLB11 yuv420p","IntLB12","IntLB13"
+    video_nr=40,41,42,43,44,45,46,47,48,49 \
+    card_label="IntLB0", "IntLB1", "IntLB2", "IntLB3", "IntLB4", "IntLB5", "IntLB6", "IntLB7", "IntLB8", "IntLB9"
 
 # sudo modprobe v4l2loopback \
 #     video_nr= \
@@ -35,6 +41,9 @@ sudo modprobe v4l2loopback \
 #     "IntLB13" \
 
 echo "setup v4l2loopback done."
-echo "--> added IntLB0(40) .. IntLB5(45) used for rgb8"
-echo "--> added IntLB6(50) .. IntLB11(55) used for yuv420p"
-echo "--> added IntLB12(60) .. IntLB12(61)"
+echo "--> added IntLB0(40) .. IntLB9(49)"
+# echo "--> added IntLB6(50) .. IntLB11(55) used for yuv420p"
+# echo "--> added IntLB12(60) .. IntLB12(61)"
+echo "ll /dev/video*:"
+videoDeviceList=$(ls --color=always -alF /dev/video*)
+echo "$videoDeviceList"
