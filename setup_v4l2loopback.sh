0ced0c9744ad584497a3fe75ffeba5bb344a1f3b
s-light <github@s-light.eu>
2017-10-21 18:17:58 +0200

    updated scripts + testpictures

--- setup_v4l2loopback.sh
+++ setup_v4l2loopback.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# # this works:
 # sudo modprobe v4l2loopback \
-#     video_nr=40,41,42,43,44,45,50 \
-#     card_label="IntLB0 rgb8", "IntLB1 rgb8", "IntLB2 rgb8", "IntLB3 rgb8", "IntLB4 rgb8", "IntLB5 rgb8", "IntLB6 yuv420p"
+#     video_nr=40,41,42,43,44,45,46,47 \
+#     card_label="IntLB0", "IntLB1", "IntLB2", "IntLB3", "IntLB4", "IntLB5", "IntLB6", "IntLB7"
 
+# TO BE TESTED
 sudo modprobe v4l2loopback \
-    video_nr=40,41,42,43,44,45,46,47,48,49 \
-    card_label="IntLB0", "IntLB1", "IntLB2", "IntLB3", "IntLB4", "IntLB5", "IntLB6", "IntLB7", "IntLB8", "IntLB9"
+    video_nr=40,41,42,43,44,45,46,47,48 \
+    card_label="IntLB0", "IntLB1", "IntLB2", "IntLB3", "IntLB4", "IntLB5", "IntLB6", "IntLB7", "IntLB8"
 
+# # DOES NOT WORK
+# sudo modprobe v4l2loopback \
+#     video_nr=40,41,42,43,44,45,46,47,48,49 \
+#     card_label="IntLB0", "IntLB1", "IntLB2", "IntLB3", "IntLB4", "IntLB5", "IntLB6", "IntLB7", "IntLB8", "IntLB9"
+
+# does not work - seems it is not possible to split the argument values onto multiple lines from a script. in shell it works.
 # sudo modprobe v4l2loopback \
 #     video_nr= \
 #         40, \
